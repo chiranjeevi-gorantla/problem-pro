4618ceaf95046eb2c59e8c1ea78b7372de136cc3
Chiranjeevi Gorantla <78554554+chiranjeevi-gorantla@users.noreply.github.com>
2024-05-05 18:10:35 -0400

    Fix vercel postgresql prisma issue

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,17 +1,13 @@
 import { PrismaClient } from '@prisma/client';
-
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
-
-type prismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: prismaClientSingleton | undefined;
-};
-
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
-
+declare global {
+  var prisma: PrismaClient;
+}
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 export default prisma;
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
