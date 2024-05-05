e85991281d222ba20eb917ef368aaeb8d78f7251
Chiranjeevi Gorantla <78554554+chiranjeevi-gorantla@users.noreply.github.com>
2024-05-04 23:53:47 -0400

    Set up Radix UI

--- app/problems/page.tsx
+++ app/problems/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Button } from '@radix-ui/themes';
 
 const ProblemPage = () => {
-  return <div>ProblemPage</div>;
+  return (
+    <div>
+      <Button>New Problem</Button>
+    </div>
+  );
 };
 
 export default ProblemPage;
